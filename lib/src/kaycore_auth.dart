5a033d3f02d6f8913a3c61143d36e35941b615c3
Kevin Gautier Kayisire <kaykevin1@gmail.com>
2022-09-06 00:09:24 +0200

    🚧 implemented remaining methods

--- lib/src/kaycore_auth.dart
+++ lib/src/kaycore_auth.dart
@@ -5,31 +5,80 @@ class KaycoreAuth {
 
   KaycoreAuth({required this.environment});
 
-  /// Return
+  /// ## Confirm phone number OTP
+  /// This method will confirm
+  /// if inputed OTP match the server generated OTP.
+  /// ```dart
+  /// confirmPhoneNumberOtp(
+  ///   identifier: 'email@email.ext',
+  ///   otp: '123123',
+  /// );
+  /// ```
   Future<dynamic> confirmPhoneNumberOtp({
     required String identifier,
     required String otp,
   }) async {
     try {
-      return UnimplementedError('confirmPhoneNumberOtp');
+      final body = <String, Object>{
+        'identifier': identifier,
+        'token': otp,
+        'option': 'otp'
+      };
+
+      return post(
+        environment: environment,
+        path: '/v2/auth/confirm',
+        body: body,
+      );
     } catch (error) {
       UnhandledException('$error');
     }
   }
 
+  /// ## Create a new business
+  /// This method will create a new business record
+  /// in Kayko and generate a business code for it.
+  /// ```dart
+  /// createBusiness(
+  ///   companyName: 'Kayko',
+  ///   category: 'ICT',
+  ///   revenueAmount: 100.00,
+  ///   revenueCurrency: 'RWF',
+  /// );
+  /// ```
   Future<dynamic> createBusiness({
+    required String userId,
     required String companyName,
     required String category,
     double? revenueAmount,
     String? revenueCurrency,
   }) async {
     try {
-      return UnimplementedError('createBusiness');
+      final body = <String, Object>{
+        'user': userId,
+        'name': companyName,
+        'category': category,
+        'revenueAmount': revenueAmount ?? 0,
+        'revenueCurrency': revenueCurrency ?? 'RWF',
+      };
+
+      return post(
+        environment: environment,
+        path: '/v2/businesses',
+        body: body,
+      );
     } catch (error) {
       UnhandledException('$error');
     }
   }
 
+  /// ## Forget password
+  /// This method will generate a random token
+  /// for the user and send it on their email to allow
+  /// them use it when setting up a new password.
+  /// ```dart
+  /// forgetPassword(identifier: 'email@email.ext');
+  /// ```
   Future<dynamic> forgetPassword({required String identifier}) async {
     try {
       final body = <String, Object>{
@@ -46,6 +95,15 @@ class KaycoreAuth {
     }
   }
 
+  /// ## Sign-in with email
+  /// This method will sign in a user using their email address
+  /// and return a JWT as well as logged in user details.
+  /// ```dart
+  /// localSignInWithEmail(
+  ///   identifier: 'email@email.ext',
+  ///   password: '123123',
+  /// );
+  /// ```
   Future<dynamic> localSignInWithEmail({
     required String identifier,
     required String password,
@@ -54,25 +112,12 @@ class KaycoreAuth {
       final body = <String, Object>{
         'identifier': identifier,
         'password': password,
-      };
-
-      return post(environment: environment, path: '/auth/login', body: body);
-    } catch (error) {
-      UnhandledException('$error');
-    }
-  }
-
-  Future<dynamic> localSignInWithPhoneNumber(
-      {required String identifier}) async {
-    try {
-      final body = <String, Object>{
-        'identifier': identifier,
-        'password': 'randompassword',
+        'mode': 'email',
       };
 
       return post(
         environment: environment,
-        path: '/auth/login',
+        path: '/v2/auth/login',
         body: body,
       );
     } catch (error) {
@@ -80,6 +125,44 @@ class KaycoreAuth {
     }
   }
 
+  /// ## Sign-in with phone number
+  /// This method will sign in a user using their phone number
+  /// and return a successful message that an OTP was sent
+  /// on the user phone number.
+  /// ```dart
+  /// localSignInWithPhoneNumber(identifier: '2507********');
+  /// ```
+  Future<dynamic> localSignInWithPhoneNumber(
+      {required String identifier}) async {
+    try {
+      final body = <String, Object>{
+        'identifier': identifier,
+        'mode': 'phone',
+      };
+
+      return post(
+        environment: environment,
+        path: '/v2/auth/login',
+        body: body,
+      );
+    } catch (error) {
+      UnhandledException('$error');
+    }
+  }
+
+  /// ## Register new account with email
+  /// This method will register a new user account using their email address
+  /// and return a JWT as well as logged in user details.
+  /// ```dart
+  /// localSignUpWithEmail(
+  ///   identifier: 'email@email.ext',
+  ///   password: '123123',
+  ///   firstname: 'Keza',
+  ///   lastname: 'Majyambere',
+  ///   country: '250',
+  ///   phoneNumber: '2507********',
+  /// );
+  /// ```
   Future<dynamic> localSignUpWithEmail({
     required String identifier,
     required String password,
@@ -89,12 +172,39 @@ class KaycoreAuth {
     String? phoneNumber,
   }) async {
     try {
-      return UnimplementedError('localSignUpWithEmail');
+      final body = <String, Object>{
+        'identifier': identifier,
+        'password': password,
+        'firstname': firstname,
+        'lastname': lastname,
+        'country': country ?? '',
+        'phoneNumber': phoneNumber ?? '',
+        'mode': 'email',
+      };
+
+      return post(
+        environment: environment,
+        path: '/v2/auth/register',
+        body: body,
+      );
     } catch (error) {
       UnhandledException('$error');
     }
   }
 
+  /// ## Register new account with phone number
+  /// This method will register a new user account using their phone number
+  /// and return a successful message that an OTP was sent
+  /// on the user phone number.
+  /// ```dart
+  /// localSignUpWithPhoneNumber(
+  ///   identifier: '2507********',
+  ///   firstname: 'Keza',
+  ///   lastname: 'Majyambere',
+  ///   country: '250',
+  ///   phoneNumber: '2507********',
+  /// );
+  /// ```
   Future<dynamic> localSignUpWithPhoneNumber({
     required String identifier,
     required String firstname,
@@ -103,12 +213,36 @@ class KaycoreAuth {
     String? phoneNumber,
   }) async {
     try {
-      return UnimplementedError('localSignUpWithPhoneNumber');
+      final body = <String, Object>{
+        'identifier': identifier,
+        'firstname': firstname,
+        'lastname': lastname,
+        'country': country ?? '',
+        'phoneNumber': phoneNumber ?? '',
+        'mode': 'phone',
+      };
+
+      return post(
+        environment: environment,
+        path: '/v2/auth/register',
+        body: body,
+      );
     } catch (error) {
       UnhandledException('$error');
     }
   }
 
+  /// ## Reset password
+  /// This method will compliment the forgetPassword method and reset
+  /// the account password with the new provided password as well as
+  /// the received OTP if it matches the one generated from the server.
+  /// ```dart
+  /// resetPassword(
+  ///   identifier: 'email@email.ext',
+  ///   password: '123123',
+  ///   otp: '123123',
+  /// );
+  /// ```
   Future<dynamic> resetPassword({
     required String identifier,
     required String password,
@@ -131,15 +265,37 @@ class KaycoreAuth {
     }
   }
 
+  /// An override of the toString() method
+  /// to display which environment was selected.
   @override
   String toString() => 'KaycoreAuth($environment)';
 
+  /// ## Verify new account
+  /// This method will verify authenticity of the new user account
+  /// by taking the identifier and OTP and cross check them with ones
+  /// on the server.
+  /// ```dart
+  /// verifyAccount(
+  ///   identifier: 'email@email.ext',
+  ///   otp: '123123',
+  /// );
+  /// ```
   Future<dynamic> verifyAccount({
     required String identifier,
     required String otp,
   }) async {
     try {
-      return UnimplementedError('verifyAccount');
+      final body = <String, Object>{
+        'identifier': identifier,
+        'token': otp,
+        'option': 'account'
+      };
+
+      return post(
+        environment: environment,
+        path: '/v2/auth/confirm',
+        body: body,
+      );
     } catch (error) {
       UnhandledException('$error');
     }
